5917e7a4b220830a62aae392bfcb9192614cfbd3
Bhavya Sehgal <bsehgal@kth.se>
2025-04-11 02:38:41 +0200

    added firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChctzR19-tfhJznpsDzG1_ah0S9V25uso",
+  authDomain: "quica-6870a.firebaseapp.com",
+  projectId: "quica-6870a",
+  storageBucket: "quica-6870a.firebasestorage.app",
+  messagingSenderId: "144970803127",
+  appId: "1:144970803127:web:42c180c6cf1b29da422a32",
+  measurementId: "G-DK4M9LE3G2",
+};
+
+export default firebaseConfig;
